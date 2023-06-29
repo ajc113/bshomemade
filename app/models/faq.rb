8dc36ea5e4e75357b3d8b776893fec55a4820cec
gullariz12 <gullariz82@gmail.com>
2023-06-29 23:48:10 +0500

    Incorporated feedback

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -7,11 +7,15 @@ class Faq < ApplicationRecord
 
   acts_as_list
 
-  after_save :update_faq_schema
+  after_commit :update_faq_schema
 
   private
 
   def update_faq_schema
     Rails.cache.delete('faq_schema')
+
+    Rails.cache.fetch('faq_schema') do
+      FaqSchemaGenerator.new.generate_schema(Faq.order(position: :asc))
+    end
   end
 end
