c32f5d1348bb26dc08e8c2722f4226aa4487b6ac
AJ Costa <costa.aj@gmail.com>
2024-08-27 21:39:17 -0400

    trailer lead form working save to db

--- app/controllers/trailers_controller.rb
+++ app/controllers/trailers_controller.rb
@@ -1,18 +1,66 @@
 class TrailersController < InheritedResources::Base
 
-  def new
-    @trailer_lead = Trailer.new
-  end
+def index
+@trailer_leads = Flavor.all
+end
 
-  def create
-    @trailer_lead = CreateTrailer.new(params).call if verify_captcha?
+def show
+end
 
-    if verify_captcha? && @trailer_lead&.valid?
-      redirect_to new_trailer_lead_path, notice: 'Your request has been submitted successfully.'
-    else
-      render :handle_error, status: :unprocessable_entity
+
+def new
+  @trailer_lead = Trailer.new
+end
+
+def edit
+end
+
+
+def create
+  @trailer_lead = Trailer.new(trailer_params)
+
+
+    respond_to do |format|
+      if @trailer_lead.save
+        format.html { redirect_to @trailer_lead, notice: 'Thank You.  Your inquiry was successfully submit.' }
+        format.json { render :show, status: :created, location: @trailer_lead }
+      else
+        format.html { render :new }
+        format.json { render json: @trailer_lead.errors, status: :unprocessable_entity }
+      end
+end
+end
+
+  def update
+    respond_to do |format|
+      if @trailer_lead.update(trailer_params)
+        format.html { redirect_to @trailer_lead, notice: 'Inquiry was successfully updated.' }
+        format.json { render :show, status: :ok, location: @trailer_lead }
+      else
+        format.html { render :edit }
+        format.json { render json: @trailer_lead.errors, status: :unprocessable_entity }
+      end
     end
   end
+
+
+  def destroy
+    @trailer_lead.destroy
+    respond_to do |format|
+      format.html { redirect_to trailers_url, notice: 'Inquiry was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
 
 
 
@@ -23,7 +71,13 @@ class TrailersController < InheritedResources::Base
   private
 
     def trailer_params
-      params.require(:trailer).permit()
+      params.require(:trailer).permit(:firstname, :lastname, :detail)
     end
 
-end
+    def verify_captcha?
+    @recaptcha_result = true if params[:recaptcha_result] == 'verified'
+    return @recaptcha_result if @recaptcha_result
+
+    @recaptcha_result = verify_recaptcha action: 'contact_form', minimum_score: 0.7, secret_key: Rails.application.credentials.recaptcha_secret_key_v3
+  end
+
--- app/models/trailer.rb
+++ app/models/trailer.rb
@@ -1,2 +1,16 @@
 class Trailer < ApplicationRecord
+
+validates :firstname, :lastname, :detail, presence: true
+
+
+
+ private
+
+  # def start_at_not_in_past
+  #   if start_at.present? && start_at < Date.current
+  #     errors.add(:start_at, "must be on or after today's date")
+  #   end
+  # end
+
+
 end
