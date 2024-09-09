21a042b7f3582b94e1f4f150204a83dfc1085738
AJ Costa <costa.aj@gmail.com>
2024-09-09 12:52:48 -0400

    added trailer to active admin page

---
+++ app/admin/trailers.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register Trailer do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :firstname, :lastname, :email, :phone, :date, :address1, :address2, :city, :state, :zip, :eventstart, :guest, :detail
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+end
