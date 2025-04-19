1e395b1c5543f25765357885d4a7e2c4e70229ec
Kartikey Tanna <tannakartikey@gmail.com>
2025-04-19 14:23:07 +0530

    Added mailer preview

--- test/mailers/previews/trailer_mailer_preview.rb
+++ test/mailers/previews/trailer_mailer_preview.rb
@@ -11,4 +11,21 @@ class TrailerMailerPreview < ActionMailer::Preview
     TrailerMailer.notify_shop
   end
 
+  # Preview this email at http://localhost:3000/rails/mailers/trailer_mailer/acknowledgment
+  def acknowledgment
+    trailer = Trailer.last || Trailer.new(
+      firstname: "Sample",
+      lastname: "User",
+      email: "sample@example.com",
+      date: Date.today,
+      eventstart: Time.now,
+      address1: "123 Main St",
+      city: "Sample City",
+      state: "MA",
+      zip: "12345",
+      guest: 50
+    )
+    TrailerMailer.acknowledgment(trailer)
+  end
+
 end
