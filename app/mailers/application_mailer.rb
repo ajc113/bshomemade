39eb34eafcb7e37e3f76488393349f5b334760df
Kartikey Tanna <tannakartikey@gmail.com>
2025-04-23 00:22:11 +0530

    Added default reply-to address in mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "B's Icecream Truck <hello@bhomemade.com>"
+  default from: "B's Icecream Truck <hello@bhomemade.com>", reply_to: 'Bs Icecream Truck <bsicecreamtruck@gmail.com>'
   
 
   layout 'mailer'
